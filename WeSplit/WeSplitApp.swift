19171ddcc48e165c0637678f09e7c850f288a080
Filipe Fernandes <filipeferibeiro@gmail.com>
2025-06-11 15:25:41 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Filipe Fernandes on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
